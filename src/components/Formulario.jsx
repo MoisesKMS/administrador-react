f03fcf3c87c55e2ccb6920553dfbd269a61dcc7b
Moises Jimenez <MoisesJimenez@Yandex.com>
2022-02-04 15:36:56 -0600

    1.1.0
    Agregado el formulario para agregar citas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,68 @@
 function Formulario() {
     return (
-        <div>
-            <h2>Formulario</h2>
+        <div className="md:w-1/2 lg:w-2/5">
+            <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+            <p className="text-lg mt-5 text-center mb-10">
+                Añade pacientes y {''}
+                <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
+
+            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+                <div className="mb-5">
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="mascota">Nombre Mascota:</label>
+                    <input
+                        type="text"
+                        placeholder="Nombre de la Mascota"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        id="mascota"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="propietario">Nombre Propietario:</label>
+                    <input
+                        type="text"
+                        placeholder="Nombre del Propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        id="propietario"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        placeholder="Email contacto Propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        id="email"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="email">Alta:</label>
+                    <input
+                        type="date"
+                        className="border-2 w-full p-2 mt-2 rounded-md"
+                        id="alta"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="sintomas">Sintomas:</label>
+                    <textarea
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        id="sintomas"
+                        placeholder="Describe los sintomas"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                    value="Agregar paciente"
+                />
+            </form>
         </div>
     )
 }
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,8 @@
 function ListadoPacientes() {
     return (
-        <>
+        <div className="md:w-1/2 lg:w-2/5">
             <h2>Listado pacientes</h2>
-        </>
+        </div>
     )
 }
 export default ListadoPacientes;
